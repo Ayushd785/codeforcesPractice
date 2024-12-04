e6856a911e06d359d85665f2bab4e8ee9df29adf
Ayushd785 <ayushd785@gmail.com>
2024-12-05 01:29:27 +0530

    corrected the logic

--- 800Rating/A1900.java
+++ 800Rating/A1900.java
@@ -1,40 +1,38 @@
-import java.util.Scanner;
-import java.util.Stack;
+    import java.util.Scanner;
 
-public class A1900{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while(T>0){
-            int n = sc.nextInt();
-            String s = sc.next();
-            int dot = 0;
-            Stack<Integer> st = new Stack<>();
-            for(int i = 0;i<n;i++){
-                char c = s.charAt(i);
-                if(c == '.'){
-                    dot++;
+    public class A1900{
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int T = sc.nextInt();
+            while(T>0){
+                int n = sc.nextInt();
+                String s = sc.next();
+                int count = 0;
+                int dots = 0;
+                boolean ans = false;
+                for(int i = 0;i<n;i++){
+                    char c = s.charAt(i);
+                    if(c == '.'){
+                        count++;
+                        dots++;
+                        if(count>=3){
+                            ans = true;
+                        }
+                    }
+                    else{
+                        count = 0;
+                    }
                 }
-                if(c!='#' && st.size()<3){
-                    st.push(1);
-                }
-            }
-                if(st.size()==3){
-                    System.out.print(2);
-                    
-                }
-                else if(st.size() ==0){
-                    System.out.print(0);
-                    
+                if(!ans){
+                    System.out.print(dots);
                 }
                 else{
-                    System.out.println(dot);
-                   
+                    System.out.println(2);
+
                 }
-            T--;
-
+                
+                T--;
+            
+            }
         }
-
-        sc.close();
     }
-}
