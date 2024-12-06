9580ca6d4afff0060f863a7feaea72cff9c25e9d
Ayushd785 <ayushd785@gmail.com>
2024-12-06 23:13:47 +0530

    updated 1896A

--- 800Rating/A1896.java
+++ 800Rating/A1896.java
@@ -1,31 +1,23 @@
 import java.util.*;
-public class A1896{
+
+public class A1896 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t>0){
-            int n = sc.nextInt();
-            int [] arr = new int[n];
-            for(int i = 0;i<n;i++){
+        int t = sc.nextInt(); // Number of test cases
+        while (t > 0) {
+            int n = sc.nextInt(); // Length of the array
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
                 arr[i] = sc.nextInt();
             }
-            boolean ans = true;
-            for(int i = 0;i<n;i++){
-                if(Math.abs((arr[i]-1)-i) == 1 || Math.abs((arr[i]-1)-i) == 0 ){
-                    ans = true;
-                }
-                else{
-                    ans = false;
-                    break;
-                }
-            }
-            if(ans){
+            
+            if(arr[0] ==1){
                 System.out.println("YES");
             }
             else{
                 System.out.println("NO");
             }
             t--;
-        }
+    }
     }
 }
